6982af7f212b210fa28003a8a2538c2acaa032d4
mtrybus2208 <mtrybus2208@wp.pl>
2020-02-11 23:31:32 +0100

    Add Yup lib for formik validation

--- src/pages/PasswordReset/PasswordReset.types.ts
+++ src/pages/PasswordReset/PasswordReset.types.ts
@@ -1,3 +1,4 @@
 export interface PasswordResetFormValues {
   newPassword: string;
+  newPasswordRepeat: string;
 }
--- src/pages/PasswordReset/components/PasswordResetForm/PasswordResetForm.tsx
+++ src/pages/PasswordReset/components/PasswordResetForm/PasswordResetForm.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import { Formik, Field, Form, FormikHelpers, FormikProps } from 'formik';
+import { Formik, Field, Form, FormikHelpers, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 import FormWrapper from '../../../../components/FormWrapper/FormWrapper';
 import FieldInput from '../../../../shared/components/FieldInput/FieldInput';
 import FieldLabel from '../../../../shared/components/FieldLabel/FieldLabel';
 import Button from '../../../../shared/components/Buttons/Button/Button';
 import { PasswordResetFormValues } from '../../PasswordReset.types';
+
+const Schema = Yup.object().shape({
+  newPassword: Yup.string().required('This field is required'),
+  newPasswordRepeat: Yup.string().when('newPassword', {
+    is: val => (val && val.length > 0 ? true : false),
+    then: Yup.string().oneOf(
+      [Yup.ref('newPassword')],
+      'Both password need to be the same',
+    ),
+  }),
+});
+
+const Error = ({ children }) => <div style={{ color: 'red' }}>{children}</div>;
 
 export interface PasswordResetFormProps {
   onSubmit: (
@@ -17,6 +31,11 @@ export interface PasswordResetFormProps {
 const PasswordResetForm: React.FunctionComponent<PasswordResetFormProps> = ({
   onSubmit,
 }) => {
+  const initialValues: PasswordResetFormValues = {
+    newPassword: '',
+    newPasswordRepeat: '',
+  };
+
   const handleSubmit = (
     values: PasswordResetFormValues,
     actions: FormikHelpers<PasswordResetFormValues>,
@@ -24,16 +43,26 @@ const PasswordResetForm: React.FunctionComponent<PasswordResetFormProps> = ({
     actions.setSubmitting(false);
     onSubmit(values, actions);
   };
-
   return (
     <Formik
-      initialValues={{ newPassword: '', newPasswordRepeat: '' }}
+      initialValues={initialValues}
       onSubmit={handleSubmit}
-      render={(props: FormikProps<PasswordResetFormValues>) => (
+      validationSchema={Schema}
+    >
+      {({
+        values,
+        errors,
+        handleSubmit,
+        handleChange,
+        handleBlur,
+        touched,
+      }) => (
         <FormWrapper name="Password reset">
           <Form>
             <FieldLabel htmlFor="newPassword" name="new password" />
             <Field type="text" name="newPassword" component={FieldInput} />
+            <ErrorMessage component={Error} name="newPassword" />
+
             <FieldLabel
               htmlFor="newPasswordRepeat"
               name="repeat new password"
@@ -43,11 +72,13 @@ const PasswordResetForm: React.FunctionComponent<PasswordResetFormProps> = ({
               type="newPasswordRepeat"
               component={FieldInput}
             />
+            <ErrorMessage component={Error} name="newPasswordRepeat" />
+
             <Button type="submit">Submit</Button>
           </Form>
         </FormWrapper>
       )}
-    />
+    </Formik>
   );
 };
 
