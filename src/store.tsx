6e7e0cf5a647d6a190b578f1a6855a7492973b40
Michał Trybus <mtrybus@pgs-soft.com>
2020-04-10 16:27:33 +0200

    Added redux-persist lib

--- src/store.tsx
+++ src/store.tsx
@@ -1,17 +1,21 @@
 import { createStore, applyMiddleware } from 'redux';
-import persistState from 'redux-localstorage';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './reducers/index';
 
-const enhancers = composeWithDevTools(
-  // persistState("auth"),
-  applyMiddleware(thunk),
-);
-
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  whitelist: ['auth'],
+};
+const pReducer = persistReducer(persistConfig, rootReducer);
+const enhancers = composeWithDevTools(applyMiddleware(thunk));
 const defaultState = {};
 
-const store = createStore(rootReducer, defaultState, enhancers);
+const store = createStore(pReducer, defaultState, enhancers);
+export const persistor = persistStore(store);
 
 export default store;
